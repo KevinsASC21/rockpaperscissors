87f2de34802b3416ee7bca863def68dd67acca3f
Glitch (rockpaperproject) <none>
2021-12-08 23:18:45 +0000

    🏌⛸ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -21,16 +21,64 @@ console.log(RPSgenerator)
 if (RPSgenerator == 0) {
 computerRPS = "rock";
 } 
-if (RPSgenerator == 1) {
+else if (RPSgenerator == 1) {
 computerRPS = "paper";
 }
-if (RPSgenerator == 2) {
+else if (RPSgenerator == 2) {
 computerRPS = "scissors";
 }
 
 computerTagRPS = "<p>" + computerRPS + "</p>";
 $(".computerChoice").append(computerTagRPS);
 
+
+if (computerRPS === userRPS) {
+winCondition = "Tie!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else if (computerRPS == "rock" && userRPS.toLowerCase()  == "scissors") {
+winCondition = "Computer Wins!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else if (computerRPS == "rock" && userRPS.toLowerCase()  == "paper") {
+winCondition = "User Wins!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else if (computerRPS == "scissors" && userRPS.toLowerCase()  == "paper") {
+winCondition = "Computer Wins!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else if (computerRPS == "scissors" && userRPS.toLowerCase()  == "rock") {
+winCondition = "User Wins!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else if (computerRPS == "paper" && userRPS.toLowerCase()  == "rock") {
+winCondition = "Computer Wins!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else if (computerRPS == "paper" && userRPS.toLowerCase()  == "scissors") {
+winCondition = "User Wins!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+
+else if (userRPS == "") {
+winCondition = "Please enter a value!";
+$(".printedResult").append("<p>" + winCondition + "</p>");
+}
+  
+else {
+$(".printedResult").append("<p>" + "Invaild Input. Try Again." + "</p>");
+}
+
+// find a way to make this more efficent, Arielle suggested 
+// using an object to store the win conditions of RPS and then acessing them
+// through the conditional statements (+ research switch statements?)
 
 
 });
